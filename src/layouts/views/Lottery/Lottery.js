fa60c67781119d202f1503eecb43d92d6d71a8f9
Luke Alousis <lucasalousis@outlook.com>
2018-08-13 20:56:01 -0400

    Lottery Tutorial Complete

--- src/layouts/views/Lottery/Lottery.js
+++ src/layouts/views/Lottery/Lottery.js
@@ -1,7 +1,7 @@
 /* Home View without the use of drizzle components */
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { AccountData, ContractData } from 'drizzle-react-components'
+import { AccountData, ContractData, ContractForm } from 'drizzle-react-components'
 //import { Button, Form, FormGroup, Input, Container, Row, Col} from 'reactstrap'
 //resources
   //const sigUtil = require("eth-sig-util")
@@ -18,7 +18,9 @@ class Lottery extends Component {
     this.state = {
       manager: '',
       players: [],
-      balance: ''
+      balance: '',
+      value: '',
+      message: ''
     };
   }
 
@@ -29,17 +31,77 @@ class Lottery extends Component {
 
     this.setState({ manager, players, balance });
   }
+  onSubmit = async (event) => {
+    event.preventDefault();
+
+    this.setState({ message: 'Waiting on transaction success...' });
+
+    await this.contracts.Lottery.methods.enter().send({
+      from: this.props.accounts[0],
+      value: this.context.drizzle.web3.utils.toWei(this.state.value, 'ether')
+    });
+
+    this.setState({ message: 'You have been entered!' });
+  };
+
+  onClick = async () => {
+
+    this.setState({ message: 'Waiting on transaction sucess'});
+
+    await this.contracts.Lottery.methods.pickWinner().send({
+      from: this.props.accounts[0],
+    });
+    this.setState({ message: 'A winner has been picked!' });
+
+  };
 
   render() {
     return (
       <div>
         <h2>Lottery Contract</h2>
         <p>
-          This contract is managed by {this.state.manager}. 
+          This contract is managed by {this.state.manager}.
           There are currently {this.state.players.length} people entered,
           competing to win {this.context.drizzle.web3.utils.fromWei(this.state.balance, 'ether')} ether!
 
         </p>
+
+        <hr />
+
+        <form onSubmit={this.onSubmit}>
+          <h4>Want to try your luck?</h4>
+          <div>
+            <label> Amount of ether to enter</label>
+            <input
+              value={this.state.value}
+              onChange={event => this.setState({ value: event.target.value })}
+            />
+          </div>
+          <button>Enter</button>
+
+        </form>
+
+        <hr />
+
+        <h3>{this.state.message}</h3>
+
+        <hr />
+
+        <h4>Ready to pick a winner?</h4>
+        <button onClick={this.onClick}>Pick a winner!</button>
+
+        <hr />
+
+        {/* Below not working with metamask
+        <p> Or using contract form... To enter the lottery the user must submit 1 ether</p>
+        <ContractForm contract="Lottery" method="enter"  sendArgs={{ value: '1000000000000000000'}}/>
+        <br/><br/>
+        */}
+
+        <hr />
+        <p> Time to pick winner </p>
+        <ContractForm contract="Lottery" method="pickWinner"/>
+        <br/><br/>
 
       </div>
 
